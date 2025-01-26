871e3bef28f05abd007a307160ad8dfba0ecbb39
jajo560 <joseantonio.jimenez@enti.cat>
2025-01-26 19:24:52 +0100

    maincpp

---
+++ Project1/Project1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "board.h"
+#include "piece.h"
+
+using namespace std;
+
+int main() {
+    Board chessBoard;
+    chessBoard.Initialize();
+
+    bool isWhiteTurn = true;
+    string from, to;
+
+}
